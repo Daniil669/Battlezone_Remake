6347898eb407730b3f3949a7ea3d9a8011e9931a
Daniil669 <daniil.freeman669@gmail.com>
2026-01-15 09:41:11 +0200

    plan update, build script, playing with sqlite3

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail # exit on fail
+
+gcc -Wall -Wextra -g -std=c11 ./src/main.c -o battlezone $(pkg-config --cflags --libs sqlite3) # -g remove for prod
+
+# make dev and prod version of this build script (configs settings hardcode)
+# devide command into variables
+# include packages later $(pkg-config --cflag --libs sdl2 sqlite3)
+# include fallback for packages
+# possible include -MMD dependecy files
+# make game build and install or uninstall so it can be ran from terminal
+
+if [ $? -eq 0 ]; then
+    echo "Build OK -> ./battlezone"
+    ./battlezone
+else
+    echo "Build failed"
+fi
--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <sqlite3.h>
 
 int main() {
-    printf("Let's shoot\n");
+    sqlite3 *DB;
+
+    int rc = sqlite3_open("./data/highscores.db", &DB);
+    if (rc != SQLITE_OK) {
+        printf("Error open DB\n");
+        printf("%s\n", sqlite3_errmsg(DB));
+    } else {
+        printf("Connected!\n");
+    }
+    sqlite3_close(DB);
+    printf("Connection closed.\n");
     return 0;
 }
